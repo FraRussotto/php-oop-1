636a2e4bd2b41224d89973cba3646db8c8a337f0
FraRussotto <russottofrancesco1@gmail.com>
2023-11-06 15:08:34 +0100

    [edit] function setImg

--- Model/Media.php
+++ Model/Media.php
@@ -5,7 +5,7 @@ class Media{
   public $name;
   public $poster;
 
-  function __construct($_file_name, $_name){
+  public function __construct($_file_name, $_name){
     $this->file_name = $_file_name;
     $this->name = $_name;
   }
--- Model/Movie.php
+++ Model/Movie.php
@@ -5,11 +5,16 @@ class Movie{
   public $genere;
   public $timing;
   public $actors;
+  public $image;
 
   public function __construct(string $_title, string $_genere, int $_timing, array $_actors){
     $this->title = $_title;
     $this->genere = $_genere;
     $this->timing = $_timing;
     $this->actors = $_actors;
+  }
+
+  public function setImg(Media $_poster){
+    $this->image = $_poster;
   }
 };
