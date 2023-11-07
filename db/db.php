a3aff05ea72f5a66f2353d1e8d6b63f7b4d8e2fa
FraRussotto <russottofrancesco1@gmail.com>
2023-11-07 16:30:10 +0100

    [edit] v-if per mostrare le info corrispondenti

--- db/db.php
+++ db/db.php
@@ -4,6 +4,10 @@ $productions = [
   new Movie('Matrix', 'Sci-Fi', ['Keanu Reeves', 'Laurence Fishburne', 'Carrie-Anne Moss'], new Media('locandina.jpg', 'Matrix'), 133, 1999),
   new Movie('Titanic', 'Dramatic', ['Leonardo DiCaprio', 'Kate Winslet', 'Billy Zane'], new Media('locandinapg9.jpg', 'Titanic'), 194, 1998),
   new Movie('Una notte da leoni', 'Comedy', ['Bradley Cooper', 'Ed Helms', 'Zach Galifianakis', 'Heather Graham', 'Justin Bartha'], new Media('locandina-una-notte-da-leoni.jpg', 'Una notte da leoni'), 100, 2009),
-  new Movie('Il miglio verde', 'Fantasy', ['Tom Hanks', 'David Morse', 'Michael Clarke Duncan'], new Media('locandina-il-miglio-verde.jpg', 'Il miglio verde'), 188, 1999)
+  new Movie('Il miglio verde', 'Fantasy', ['Tom Hanks', 'David Morse', 'Michael Clarke Duncan'], new Media('locandina-il-miglio-verde.jpg', 'Il miglio verde'), 188, 1999),
+  new SerieTv('Lost', 'Dramatic', ['Matthew Fox', 'Evangeline Lilly', 'Dominic Monaghan', 'Ian Somerhalder'], new Media ('lost.jpg', 'Lost'), 2004, 2010, 122, 6),
+  new SerieTv('Breaking Bad', 'Thriller', ['Bryan Cranston', 'Aaron Paul', 'Anna Gunn'], new Media('breaking-bad.jpg', 'Breaking Bad'), 2008, 2013, 62, 5),
+  new SerieTv('Tutto in famiglia', 'Comedy', ['Damon Wayans', 'Tisha Campbell-Martin', 'George Gore', 'Parker McKenna Posey'], new Media ('tuttoinfamiglia.jpg', 'Tutto in famiglia'), 2001, 2005, 123, 5),
+  new SerieTv('La vita secondo Jim', 'Comedy', ['Jim Belushi', 'Courtney Thorne-Smith', 'Larry Joe Campbell', 'Taylor Atelian'], new Media ('lavitasecondojim.jpg', 'La vita secondo Jim'), 2001, 2009, 182, 8),
 ];
 
--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 require_once __DIR__ . '/Model/Productions.php';
 require_once __DIR__ . '/Model/Movie.php';
+require_once __DIR__ . '/Model/SerieTv.php';
 require_once __DIR__ . '/Model/Media.php';
 require_once __DIR__ . '/db/db.php';
 
@@ -21,20 +22,29 @@ require_once __DIR__ . '/db/db.php';
 <title>Productions</title>
 </head>
 <body>
-  <div class="container my-5 d-flex justify-content-between">
+  <div class="container my-5 d-flex flex-wrap">
 
     <?php foreach($productions as $production): ?>
 
-    <div class="card" style="width: 18rem;">
-      <img src="asset/<?php echo $production->image->file_name ?>" class="card-img-top layout-img" alt="<?php echo $production->title ?>">
+    <div class="card m-3" style="width: 18rem;">
+      <img src="asset/<?= $production->image->file_name ?>" class="card-img-top layout-img" alt="<?= $production->title ?>">
       <div class="card-body">
-        <h5 class="card-title"><?php echo $production->title ?></h5>
+        <h5 class="card-title"><?= $production->title ?></h5>
         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
       </div>
       <ul class="list-group list-group-flush">
-        <li class="list-group-item">Genere: <?php echo $production->genere ?></li>
-        <li class="list-group-item">Durata: <?php echo $production->running_time ?>'</li>
-        <li class="list-group-item">Attori: <?php echo implode(", ", $production->actors) ?></li>
+        <li class="list-group-item">Genere: <?= $production->genere ?></li>
+        <li class="list-group-item">Attori: <?= implode(", ", $production->actors) ?></li>
+        <?php if(get_class($production) == 'Movie'): ?>
+          <li class="list-group-item">Durata: <?= $production->running_time ?>'</li>
+        <?php else: ?>
+          <li class="list-group-item">Anno uscita: <?= $production->aired_from_year ?></li>
+          <li class="list-group-item">Anno ultimo episodio: <?= $production->aired_to_year ?></li>
+          <li class="list-group-item">Totale episodi: <?= $production->number_of_episodes ?></li>
+          <li class="list-group-item">Stagioni: <?= $production->number_of_seasons ?></li>
+
+          <?php endif; ?>
+
       </ul>
     </div>
 
